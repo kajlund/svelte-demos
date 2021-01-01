ae0c1409c1058d3287d4f15b57e0aedf5693ed0c
LuKa <kaj.lund@gmail.com>
2021-01-01 11:34:36 +0200

    Adds simple loan calculator demo

---
+++ loan/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let interestrate = 3;
+	let loanAmount = 200000;
+	let years = 30;
+	const MONTHS_PER_YEAR = 12;
+	$: months = years * MONTHS_PER_YEAR;
+	$: monthlyInterestRate = interestrate / 100 / MONTHS_PER_YEAR;
+	$: numerator = loanAmount * monthlyInterestRate;
+	$: denominator = 1 - (1 + monthlyInterestRate) ** -months;
+	$: payment = !loanAmount || !years ? 0 : interestrate ? numerator / denominator :
+	loanAmount / months;
+</script>
+
+<main>
+	<label for="loan">Loan Amount</label>
+	<input id="loan" type="number" bind:value={loanAmount}>
+	
+	<label for="interest">Interest Rate</label>
+	<input id="interest" type="number" bind:value={interestrate}>
+	
+	<label for="years">Years</label>
+	<input id="years" type="number" bind:value={years}>
+
+	<div>
+		Monthly Payment: ${payment.toFixed(2)}
+	</div>
+</main>
+
+<style></style>
